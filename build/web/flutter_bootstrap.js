121db92d690b1e6469c5e29e4c77c519d73b907f
muhammed shadhin <shadhinkb222@gmail.com>
2025-10-16 01:41:54 +0400

    Update website 2025-10-16 01:41

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1128489417"
+    serviceWorkerVersion: "2821236057"
   }
 });
